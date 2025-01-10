2fc5005c90afc8cae81ed900659d4a8f3409952f
Nishant Shingate <62829800+Nishant0073@users.noreply.github.com>
2025-01-10 20:59:39 +0530

    [916] [Time Beats: 53.88%] [Memory Beats: 42.47%] - LeetPush

--- DCP-01-25/916-Word-Subsets.cpp
+++ DCP-01-25/916-Word-Subsets.cpp
@@ -1,42 +1,38 @@
 class Solution {
 public:
-    bool universalOrNot(unordered_map<char,int> &mp1,unordered_map<char,int> &mp2){
-        for(int i=0;i<26;i++){
-            char ch = 'a'+i;
-            if(mp1[ch]>mp2[ch]){
-                return false;
+    vector<string> wordSubsets(vector<string>& words1, vector<string>& words2) {
+    
+        vector<int> maxFreq(26, 0);
+        for (const string& word : words2) {
+            vector<int> freq(26, 0);
+            for (char ch : word) {
+                freq[ch - 'a']++;
+            }
+            for (int i = 0; i < 26; i++) {
+                maxFreq[i] = max(maxFreq[i], freq[i]);
             }
         }
-        return true; 
-    }
-    vector<string> wordSubsets(vector<string>& words1, vector<string>& words2) {
-        //first create universal word count 
-        unordered_map<char,int> mp;
-        for(int i=0;i<words2.size();i++){
-            unordered_map<char,int> mp2;
-            string cword = words2[i];
-            for(int j=0;j<cword.size();j++){
-                mp2[cword[j]]++;
+
+        vector<string> result;
+        for (const string& word : words1) {
+            vector<int> freq(26, 0);
+            for (char ch : word) {
+                freq[ch - 'a']++;
             }
-            for(int i=0;i<26;i++){
-                if(mp['a'+i]<mp2['a'+i]){
-                    mp['a'+i] = mp2['a'+i];
+
+            bool isUniversal = true;
+            for (int i = 0; i < 26; i++) {
+                if (freq[i] < maxFreq[i]) {
+                    isUniversal = false;
+                    break;
                 }
             }
-        }
-        vector<string> result;
-        // count frq of each word in words1 
-        for(int i=0;i<words1.size();i++){
-            string cword = words1[i];
-            unordered_map<char,int> mp1;
-            for(char ch:cword){
-                mp1[ch]++;
-            }
-            //check if current word is universal or not
-            if(universalOrNot(mp,mp1)){
-                result.push_back(cword);
+
+            if (isUniversal) {
+                result.push_back(word);
             }
         }
+
         return result;
     }
 };
