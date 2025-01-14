2f89daca8d43f0434269c086b529edfeeebfbcef
Nishant Shingate <62829800+Nishant0073@users.noreply.github.com>
2025-01-14 21:22:38 +0530

    [2657] [Time Beats: 12.74%] [Memory Beats: 31.97%] - LeetPush

--- DCP-01-25/2657-Find-the-Prefix-Common-Array-of-Two-Arrays.cpp
+++ DCP-01-25/2657-Find-the-Prefix-Common-Array-of-Two-Arrays.cpp
@@ -1,24 +1,23 @@
 class Solution {
 public:
     vector<int> findThePrefixCommonArray(vector<int>& A, vector<int>& B) {
-         unordered_map<int,bool> mpA;
-         unordered_map<int,bool> mpB;
-        int cnt = 0;
+        unordered_map<int,int> mp;
+        int cnt =0;
         vector<int> result;
         for(int i=0;i<A.size();i++){
             int eleFromA = A[i];
             int eleFromB = B[i];
-            mpA[eleFromA] = true;
-            mpB[eleFromB] = true;
-            if(mpB[eleFromA]==true){
+            mp[eleFromA]++;
+            mp[eleFromB]++;
+            if(mp[eleFromA]==2){
                 cnt++;
             }
-            if(mpA[eleFromB]==true && eleFromA!=eleFromB){
+            if(mp[eleFromB]==2 && eleFromB!=eleFromA){
                 cnt++;
             }
-            
             result.push_back(cnt);
         }
         return result;
     }
 };
+
