a180ba2ce9a0e2e5bc779ca22a3445096626d848
Nishant Shingate <62829800+Nishant0073@users.noreply.github.com>
2025-01-20 20:00:48 +0530

    [518] [Time Beats: 39.55%] [Memory Beats: 10.49%] - LeetPush

--- 518-Coin-Change-II.cpp
+++ 518-Coin-Change-II.cpp
@@ -20,8 +20,25 @@ public:
     }
     int change(int amount, vector<int>& coins) {
         int n = coins.size();
-        vector<vector<int>> dp(amount+1,vector<int>(n,-1));
-        return solve(amount,coins.size()-1,coins,dp);
+        //vector<vector<int>> dp(amount+1,vector<int>(n+1,-1));
+        //return solve(amount,coins.size()-1,coins,dp);
+        vector<vector<uint64_t>> dp(n+1,vector<uint64_t>(amount+1,0));
+
+        for(int i=0;i<=n;i++){
+            dp[i][0] = 1;
+        }
+
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=amount;j++){
+               if(j-coins[i-1]>=0){
+                dp[i][j] = dp[i][j-coins[i-1]] + dp[i-1][j];
+               }
+               else{
+                  dp[i][j]+=dp[i-1][j];
+               }
+            }
+        }
+        return dp[n][amount];
 
     }
 };
