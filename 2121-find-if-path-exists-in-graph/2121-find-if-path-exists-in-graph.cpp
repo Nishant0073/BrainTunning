17702d92e11ed08a1471a7d54ad1c2589071def0
Nishant Shingate <62829800+Nishant0073@users.noreply.github.com>
2025-02-02 16:37:11 +0530

    Time: 30 ms (94.34%), Space: 209.1 MB (96.47%) - LeetHub

--- 2121-find-if-path-exists-in-graph/2121-find-if-path-exists-in-graph.cpp
+++ 2121-find-if-path-exists-in-graph/2121-find-if-path-exists-in-graph.cpp
@@ -1,33 +1,49 @@
 #include<bits/stdc++.h>
 using namespace std;
+struct Node{
+	int parent;
+	int rank;
+};
 class Solution {
 
 public:
-	int findRoot(vector<int> &parent,int element1){
-		if(parent[element1]==-1)
-			return element1;
-		int root =  findRoot(parent, parent[element1]);
-        parent[element1] = root;
-        return root;
+	int findRoot(int element,vector<Node> &parent){
+		if(parent[element].parent==-1)
+			return element;
+		parent[element].parent = findRoot(parent[element].parent,parent);
+        return parent[element].parent;
 	}
-	
-	void groupUnion(vector<int> &parent,int element1,int element2){
-		int root1 = findRoot(parent,element1);
-		int root2 = findRoot(parent,element2);
 
-		if(root1==root2)
-			return;
-		parent[root1] = root2;
-
+	void groupUnion(int root1,int root2,vector<Node> &parent){
+		if(parent[root1].rank>parent[root2].rank){
+			parent[root2].parent = root1;
+		}
+		else if(parent[root1].rank < parent[root2].rank){
+			parent[root1].parent = root2;
+		}
+		else{
+			parent[root1].parent = root2;
+			parent[root2].rank++;
+		}
 	}
 	bool validPath(int n, vector<vector<int>>& edges, int source, int destination) {
-		vector<int> parent(n,-1);
+		vector<Node> parent(n);
+		for(int i=0;i<n;i++){
+			parent[i].parent = -1;
+			parent[i].rank = 0;
+		}
 
 		for(int i=0;i<edges.size();i++){
-			groupUnion(parent,edges[i][0],edges[i][1]);
+			int root1 = findRoot(edges[i][0],parent);
+			int root2 = findRoot(edges[i][1],parent);
+			if(root1!=root2)
+            {
+              groupUnion(root1,root2,parent);
+            }
 		}
-		int root1 = findRoot(parent,source);
-		int root2 = findRoot(parent,destination);
+
+        int root1 = findRoot(source,parent);
+        int root2 = findRoot(destination,parent);
 		return root1==root2;
 	}
 };
